47c1f204417a3347f73a8855dcc2c80b03a84d10
Ronny Gutierrez <ronny.icne@gmail.com>
2020-10-21 20:54:37 -0700

    Created the admin customers pages

---
+++ resources/views/pages/reservations.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    <div id="waitlist-page">
+
+
+        <div class="content-box">
+            <div class="row">
+                <div class="col-md-6">
+                <h1>Get On The List</h1>
+                <form>
+                    <div class="form-group">
+                      <label for="firstnameinput">First Name</label>
+                      <input name="fname" type="text" class="form-control" id="firstnameinput" placeholder="John">
+                    </div>
+                    <div class="form-group">
+                      <label for="lastnameinput">Last Name</label>
+                      <input name="lname" type="text" class="form-control" id="firstnameinput" placeholder="Doe">
+                    </div>
+                    <div class="form-group">
+                      <label for="emailinput">Email address</label>
+                      <input type="email" class="form-control" name="email" id="emailinput" placeholder="name@example.com">
+                    </div>
+                    <div class="form-group">
+                        <label for="phoneinput">Phone Number</label>
+                        <input name="phone" type="text" class="form-control" id="phoneinput" placeholder="(555) 555-5555">
+                    </div>
+                    <div class="form-group">
+                      <label for="guestsinput">How Many Guests</label>
+                      <select name="guests" class="form-control" id="guestsinput">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                      </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="timeinput">What Time</label>
+                        <select name="time" class="form-control" id="timeinput">
+                          <option value="6">6:00 PM</option>
+                          <option value="7">7:00 PM</option>
+                          <option value="8">8:00 PM</option>
+                          <option value="9">9:00 PM</option>
+                          <option value="10">10:00 PM</option>
+                        </select>
+                      </div>
+                      <div class="form-group">
+                        <button type="submit" class="btn btn-primary mb-2">Save</button>
+                      </div>
+                  </form>
+                </div>
+                <div class="col-md-6">
+                    <p>
+                    Please Note: This is not a reservation. You will be added tot he current wait list. You have a short wait once you arrive while we prepare your table.
+                    </p>
+                </div>
+            </div>       
+        </div>
+    </div>
+@endsection
