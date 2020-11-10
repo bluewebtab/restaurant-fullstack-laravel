94a3c96b3651ad25863eb75f12b783ddd0a8acf8
Ronny Gutierrez <ronny.icne@gmail.com>
2020-11-09 19:50:30 -0800

    added feature to create food item and worked on controller

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,11 @@ Route::delete('/admin/food-categories/{id}/delete', 'admin\FoodCategoriesControl
 //Admin Food Items
 Route::get('/admin/food-items', 'admin\FoodItemsController@index');
 Route::get('/admin/food-items/create', 'admin\FoodItemsController@create');
+Route::post('/admin/food-items', 'admin\FoodItemsController@store');
 Route::get('/admin/food-items/{id}/edit', 'admin\FoodItemsController@edit');
+Route::put('/admin/food-items/{id}', 'admin\FoodItemsController@update');
+Route::delete('/admin/food-items/{id}/delete', 'admin\FoodItemsController@delete');
+
 
 //Admin Customers
 Route::get('/admin/offers-members', 'admin\CustomersController@allOffersMembers');
