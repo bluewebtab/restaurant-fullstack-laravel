c23e600cd0f4a33094e34d716aadf904fd4c27c1
Ronny Gutierrez <ronny.icne@gmail.com>
2020-12-12 12:17:43 -0800

    added seo tags to head

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,21 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-        <title>Laravel</title>
+        
+        <title>{{$settings["general"]->site_title}}</title>
+        <meta name="description"
+          content="{{$settings["seo"]->description}}">
+        <meta name="robots" content="follow,index,max-snippet:-1,max-video-preview:-1,max-image-preview:large">
+        {{-- <link rel="canonical" href=""> --}}
+        <meta property="og:locale" content="en_US">
+        <meta property="og:type" content="website">
+        <meta property="og:title" content="{{$settings["general"]->site_title}}">
+        <meta property="og:description" content="{{$settings["seo"]->description}}">
+        {{-- <meta property="og:url" content=""> --}}
+        <meta property="og:site_name" content="{{$settings["general"]->site_title}}">
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="{{$settings["general"]->site_title}}">
+        <meta name="twitter:description" content="{{$settings["seo"]->description}}">
 
          <!-- CSRF Token -->
          <meta name="csrf-token" content="{{ csrf_token() }}">
